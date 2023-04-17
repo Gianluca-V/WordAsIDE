388568e7847a211d6d7f3a353ff29ef0f29e6b52
Gianluca-V <vespe.gianluca@gmail.com>
2023-04-17 00:25:14 -0300

    control panel text changed to english

--- WordAsIDE/Form1.Designer.cs
+++ WordAsIDE/Form1.Designer.cs
@@ -40,7 +40,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(263, 186);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Compilar";
+            this.button1.Text = "Compile";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.compileTextOnClick);
             // 
@@ -51,18 +51,18 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(263, 186);
             this.button2.TabIndex = 2;
-            this.button2.Text = "Ejecutar";
+            this.button2.Text = "Execute";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.ExecuteButtonOnClick);
             // 
             // button3
             // 
-            this.button3.Font = new System.Drawing.Font("Agency FB", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.Font = new System.Drawing.Font("Agency FB", 44F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button3.Location = new System.Drawing.Point(492, 41);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(263, 186);
             this.button3.TabIndex = 3;
-            this.button3.Text = "Compilar y ejecutar";
+            this.button3.Text = "Compile and execute";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.CompileExecuteOnCLick);
             // 
